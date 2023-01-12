60af27d5fd879a87b8d4e78ea75aec0bc213229d
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-01-12 14:22:24 +0800

    Make `ctest` output on failure

--- build-mold.sh
+++ build-mold.sh
@@ -20,7 +20,7 @@ cd "$SRCDIR/mold"
 git clone https://github.com/rui314/mold.git --branch "$MOLD_GIT_REF" --depth 1 .
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DMOLD_MOSTLY_STATIC=ON
 cmake --build build -j "$(nproc)"
-ctest --test-dir build -j "$(nproc)"
+ctest --test-dir build -j "$(nproc)" --output-on-failure
 cmake --install build --prefix "$OPTDIR/mold$BUILD_SUFFIX" --strip
 tar -C "$OPTDIR" -czf "$OUTDIR/mold$BUILD_SUFFIX.tar.gz" "mold$BUILD_SUFFIX"
 rm -rf "$SRCDIR/mold" "$OPTDIR/mold$BUILD_SUFFIX"
